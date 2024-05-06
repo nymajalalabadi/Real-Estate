716160ede50fb1cb25ffde348befdf7695f197c9
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-05-06 15:24:45 +0330

    LogOut Completed

--- RealEstate-Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ RealEstate-Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -16,26 +16,22 @@ namespace RealEstate_Web.Areas.Identity.Pages.Account
     public class LogoutModel : PageModel
     {
         private readonly SignInManager<UserModel> _signInManager;
-        private readonly ILogger<LogoutModel> _logger;
 
-        public LogoutModel(SignInManager<UserModel> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<UserModel> signInManager)
         {
             _signInManager = signInManager;
-            _logger = logger;
         }
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");
+
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
             }
             else
             {
-                // This needs to be a redirect so that the browser performs a new
-                // request and the identity for the user gets updated.
                 return RedirectToPage();
             }
         }
